af7326b7314a314a2dcad07c015e218b422d08d5
Rasya Aryasatya <luke.genesis.hyder@gmail.com>
2023-07-26 19:07:09 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const openInNewWindow = event => {
+//     event.preventDefault();
+//     const {href, target} = event.currentTarget;
+//     const features = "resizable";
+//     window.open(href, target, features);
+//   };
+  
+  
+//   document.querySelector("button")
+//     .addEventListener("click", openInNewWindow);
